cc5e29e9d168782147bcb140ef999dde0bd4a62e
Jaskieeer <troxipl30@gmail.com>
2022-03-19 12:31:09 +0100

    qfx

--- code/models/store.py
+++ code/models/store.py
@@ -1,10 +1,10 @@
 from db import db
 
 class StoreModel(db.Model):
-    
+
     __tablename__= 'stores'
     id= db.Column(db.Integer, primary_key=True,autoincrement=True)
-    name= db.Column(db.Text(20))
+    name= db.Column(db.String(20))
     items = db.relationship('ItemModel',lazy = 'dynamic')
 
     def __init__(self,name):
