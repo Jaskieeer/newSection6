679f0d1772763fd849697539ec7d82183e36c5a0
Jaskieeer <troxipl30@gmail.com>
2022-03-19 12:25:35 +0100

    qfx

--- code/app.py
+++ code/app.py
@@ -11,7 +11,7 @@ from db import db
 
 #test
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"]= os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config["SQLALCHEMY_DATABASE_URI"]= os.environ.get('DATABASE_URI', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
 app.config['PROPAGATE_EXCEPTIONS'] = True
 app.secret_key = 'wow'
